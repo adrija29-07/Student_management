1266d6b7441c0a287bd8c8c49f200cb21a89b209
adrija29-07 <adrijaaroy29@gmail.com>
2026-07-05 07:31:12 +0530

    Clean repository structure

--- backend/src/utils/imagekit.ts
+++
@@ -1,8 +0,0 @@
-import ImageKit from '@imagekit/nodejs';
-
-// Initialize ImageKit client
-export const imagekit = new ImageKit({
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY || 'private_G3M1KD+ELDvq3r8IVBqucEXmu6A=',
-});
-
-export default imagekit;
